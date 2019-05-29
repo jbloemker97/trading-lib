3fbf3db1885f7b565122e95d932215d088a11363
Jacob Bloemker <jbloemker97@gmail.com>
2019-05-29 19:35:55 -0400

    Added permission variable. Creating dir if doesnt exist

--- WriteCSV.py
+++ WriteCSV.py
@@ -6,11 +6,23 @@ class WriteCSV:
     def __init__(self, filename, pubdir=None):
         self.filename = filename
         self.pubdir = pubdir
+
+    def clean_path(self):
+        # Is directory doesn't end with / and filename doesn't start with a /
+        if self.pubdir[-1] is not "/" and self.filename[0] is not "/":
+            self.pubdir = self.pubdir + "/"
     
     def file_path(self):
         if self.pubdir is not None:
-            os.chdir(self.pubdir)
-            return os.path.join(os.getcwd(), self.filename)
+            self.clean_path()
+            try:
+                os.makedirs(os.path.join(os.getcwd(), self.pubdir))
+            except OSError as error:
+                if os.path.isdir(self.pubdir):
+                    pass
+                else: raise
+
+            return os.path.join(os.getcwd(), self.pubdir + self.filename)
         return os.path.join(os.getcwd(), self.filename)
 
     def write_list(self, header_row, data):
@@ -25,10 +37,13 @@ class WriteCSV:
             
             writer.writerow(data)
 
-    def write_dict(self, data):
+    def write_dict(self, data, permission = "a"):
         is_file = os.path.isfile(self.file_path())
 
-        with open(self.file_path(), "a") as file:
+        
+        # if self.pubdir is not None:
+
+        with open(self.file_path(), permission) as file:
             writer = csv.DictWriter(file, fieldnames=data.keys(), lineterminator = '\n')
             
             # Write header if file (or headers) don't exist
