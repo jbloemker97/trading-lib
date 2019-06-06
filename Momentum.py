56eb4e1b0594858c655ac44b5ccd9cb6f448c5de
Jacob Bloemker <jbloemker97@gmail.com>
2019-06-05 21:34:05 -0400

    Buying AAPL if rsi < 50

--- Momentum.py
+++ Momentum.py
@@ -7,12 +7,28 @@ class MomentumStrategy(strategy.BacktestingStrategy):
     def __init__(self, feed, instrument):
         super(MomentumStrategy, self).__init__(feed)
         self.__instrument = instrument
-        self.setUseAdjustedValues(True)
         self.__prices = feed[instrument].getPriceDataSeries()
-        self.__rsi = rsi.RSI(feed, 14)
+        self.__rsi = rsi.RSI(self.__prices, 14)
 
     def getRSI(self):
         return self.__rsi
 
     def onBars(self, bars):
-        pass
+        # Wait for bars to load
+        if self.__rsi[-1] is None:
+            return
+
+        bar = bars[self.__instrument]
+
+        if self.enterLongSignal(bar):
+            self.info(f"Entering Long: {bar.getPrice()}")
+
+
+    def enterLongSignal(self, bar):
+        return self.__rsi[-1] < 50 # Threshold
+
+feed = GenericBarFeed(Frequency.DAY)
+feed.addBarsFromCSV("AAPL", "data/AAPL.csv")
+
+momentumStrategy = MomentumStrategy(feed, "AAPL")
+momentumStrategy.run()
