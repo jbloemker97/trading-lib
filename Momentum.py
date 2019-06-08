3a996bc87fcd5be5a616586f819feddc819b65ab
Jacob Bloemker <jbloemker97@gmail.com>
2019-06-08 10:18:13 -0400

    Updated multiple tickers strategy

--- Momentum.py
+++ Momentum.py
@@ -4,31 +4,42 @@ from pyalgotrade.bar import Frequency
 from pyalgotrade.technical import rsi
 
 class MomentumStrategy(strategy.BacktestingStrategy):
-    def __init__(self, feed, instrument):
+    def __init__(self, feed, instruments):
         super(MomentumStrategy, self).__init__(feed)
-        self.__instrument = instrument
-        self.__prices = feed[instrument].getPriceDataSeries()
-        self.__rsi = rsi.RSI(self.__prices, 14)
+        self.__instruments = instruments
+        self.__rsi = None
+        # self.__prices = feed[instrument].getPriceDataSeries()
+        # self.__rsi = rsi.RSI(self.__prices, 14)
 
     def getRSI(self):
-        return self.__rsi
+        if self.__rsi is not None: return self.__rsi
+
+    def setValues(self, prices, rsi):
+        self.__prices = prices
+        self.__rsi = rsi
 
     def onBars(self, bars):
-        # Wait for bars to load
-        if self.__rsi[-1] is None:
-            return
 
-        bar = bars[self.__instrument]
-
-        if self.enterLongSignal(bar):
-            self.info(f"Entering Long: {bar.getPrice()}")
+        for instrument in self.__instruments:
+            bar = bars[instrument]
+            self.info(f"{instrument}: {bar.getClose()}")
 
 
     def enterLongSignal(self, bar):
         return self.__rsi[-1] < 50 # Threshold
 
-feed = GenericBarFeed(Frequency.DAY)
-feed.addBarsFromCSV("AAPL", "data/AAPL.csv")
 
-momentumStrategy = MomentumStrategy(feed, "AAPL")
-momentumStrategy.run()
+def main (instruments):
+    feed = GenericBarFeed(Frequency.DAY)
+
+    # building feed with multiple instruments
+    for instrument in instruments:
+        feed.addBarsFromCSV(instrument, f"data/{instrument}.csv")
+
+    momentumStrategy = MomentumStrategy(feed, instruments)
+    momentumStrategy.run()
+   
+
+main(['AAPL', 'TSLA', 'SPY'])
+
+
