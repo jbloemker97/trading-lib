0a675d006e551a9b3cd215c8affbbbb9034f7a7c
Jacob Bloemker <jbloemker97@gmail.com>
2019-06-08 20:42:49 -0400

    Added MACD

--- Momentum.py
+++ Momentum.py
@@ -1,7 +1,7 @@
 from pyalgotrade import strategy
 from pyalgotrade.barfeed.csvfeed import GenericBarFeed
 from pyalgotrade.bar import Frequency
-from pyalgotrade.technical import rsi, ma
+from pyalgotrade.technical import rsi, ma, macd
 
 class MomentumStrategy(strategy.BacktestingStrategy):
     def __init__(self, feed, instruments, smaPeriod):
@@ -11,23 +11,25 @@ class MomentumStrategy(strategy.BacktestingStrategy):
         self.__prices = {}
         self.__rsi = {}
         self.__ma = {}
+        self.__macd = {}
 
         for instrument in feed.getRegisteredInstruments():
             self.__prices[instrument] = feed[instrument].getPriceDataSeries()
             self.__rsi[instrument] = rsi.RSI(self.__prices[instrument], 14)
             self.__ma[instrument] = ma.SMA(self.__prices[instrument], smaPeriod)
+            self.__macd[instrument] = macd.MACD(self.__prices[instrument], 12, 26, 9).getHistogram() # Buy when historgram is > 0. Sell when histogram is < 0
 
     def onBars(self, bars):
 
         # Loop through list of stocks
         for instrument in self.__instruments:
 
-            if self.__rsi[instrument][-1] is None:
+            if self.__rsi[instrument][-1] is None and self.__ma[instrument][-1] is not None:
                 return
 
             
             bar = bars[instrument]
-            self.info(f"{instrument}: {bar.getClose()} RSI: {self.__rsi[instrument][-1]}, SMA: {self.__ma[instrument][-1]}")
+            self.info(f"{instrument}: {bar.getClose()} RSI: {self.__rsi[instrument][-1]}, SMA: {self.__ma[instrument][-1]}, MACD: {self.__macd[instrument][-1]}")
 
 
 def main (instruments):
